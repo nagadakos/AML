Fixed a word- error computation bug for part 3. Added Learning curve plots.

Assignment_2_Programming/code/data_loader.py
@@ -2,18 +2,21 @@ import gzip
 import csv
 import numpy as np
 import torch
+import torch.utils.data as data_utils
 
 class DataLoader:
-    def __init__(self, type= 'word-features'):
+    def __init__(self, dType= 'word-features'):
         data_path = '../data/letter.data.gz'
         lines = self._read(data_path)
-        if type == 'word-features':
+        self.wordIdxs = 0
+        if dType == 'word-features':
             data, target = self._parse(lines)
             #self.data, self.target = self._parse(lines)
             self.data, self.target = self._pad(data, target)
         else:
             #print(lines[0:20])
-            self.data, self.target = self._letter_parse(lines)
+            self.data, self.target = self._parse2(lines)
+            #self.data, self.target, self.wordIdxs = self._letter_parse(lines)
 
     @staticmethod
     def _read(filepath):
@@ -24,10 +27,10 @@ class DataLoader:
 
     @staticmethod
     def _parse(lines):
-        lines = sorted(lines, key=lambda x: int(x[0]))
+        #lines = sorted(lines, key=lambda x: int(x[0]))
         data, target = [], []
         next_ = None
-
+        i = 0
         for line in lines:
             if not next_:
                 data.append([])
@@ -42,11 +45,34 @@ class DataLoader:
         return data, target
     
     @staticmethod
+    def _parse2(lines):
+        lines = sorted(lines, key=lambda x: int(x[0]))
+        data, target, wordIdxs = [], [], []
+        next_ = None
+        i = 0
+        for line in lines:
+            if not next_:
+                data.append([])
+                target.append([])
+                wordIdxs.append([])
+            else:
+                assert next_ == int(line[0])
+            next_ = int(line[2]) if int(line[2]) > -1 else None
+            pixels = np.array([int(x) for x in line[6:134]])
+            pixels = pixels.reshape((16, 8))
+            data[-1].append(pixels)
+            target[-1].append(ord(line[1]) - ord('a'))
+            wordIdxs[-1].append(int(line[0])-1)
+        return data, target
+
+    
+    @staticmethod
     def _letter_parse(lines):
         lines = sorted(lines, key=lambda x: int(x[0]))
         data, target = [], []
         data   = np.zeros((len(lines),16,8))
         target = np.zeros((len(lines),1))
+        wordIdxs   = np.zeros((len(lines),1))
         next_ = None
 
         for i,line in enumerate(lines):
@@ -54,14 +80,24 @@ class DataLoader:
             pixels = pixels.reshape((16, 8))
             data[i] = pixels
             target[i] = ord(line[1]) - ord('a')
+            letterIdxs[i] = line[3]
         # One-hot encode targets.
         #target1Hot = np.zeros((len(target),26))
         #for index, letter in enumerate(target):
         #    if letter:
         #        target1Hot[index][ord(letter) - ord('a')] = 1
-        return data, target
+        return data, target, letterIdxs
+    
+    @staticmethod
+    def _get_word_idxs(lines):
+        lines = sorted(lines, key=lambda x: int(x[0]))
+        idxs   = np.zeros((len(lines),1))
+        
+        print(len(lines), len(lines[0]))
 
-
+        #for i,line in enumerate(lines):
+        return idxs
+    
     @staticmethod
     def _pad(data, target):
         """
@@ -72,13 +108,85 @@ class DataLoader:
         data = [x + ([padding] * (max_length - len(x))) for x in data]
         target = [x + ([''] * (max_length - len(x))) for x in target]
         return np.array(data), np.array(target)
-
-def get_dataset(type = 'word-features', convToTensor = False):
     
-    if type == 'word-features':
-        dataset = DataLoader(type=type)
+# -------------------------------------------------------------------------------------------------------------
+def words_to_letters(data, target):
+    """ Description: THis funciton will conver a list of words to a numpy array of letters.
+    """
+    #data = [w for w in data]
+    # [item for sublist in l for item in sublist]
+    data = [l for w in data for l in w]
+    wordLimits = np.zeros((len(target),1))
+    print(len(target))
+    for i, t in enumerate(target[1:]):
+        if(i<21):
+            print(len(t), target[i], target[100])
+        wordLimits[i+1] = wordLimits[i] + len(t)
+    target = [l for w in target for l in w]
+    return np.array(data), np.array(target), np.array(wordLimits)
 
-        print(dataset.data[0].shape)
+# -----------------------------------------------------------------
+
+def load_data(bSize = 32, dType = 'word-features', split = 0.5):
+    # bundle common args to the Dataloader module as a kewword list.
+    # pin_memory reserves memory to act as a buffer for cuda memcopy 
+    # operations
+    comArgs = {'shuffle': True,'num_workers': 1, 'pin_memory': True} if torch.cuda.is_available() else {}
+    trainWordLimits, testWordLimits = 0, 0
+    # Data Loading -----------------------
+    # ******************
+    # load the data set as a colldection of tuples. First element in tuple are the data formated as a numOFLetters / word x 128; second 
+    # elements is a one-hot endoding of the label of the letter so: numOfLetter / word x 26. Zero padding has been used to keep word size
+    # consistent to 14 letters per word. The labels for the padding rows are all 0s.
+    # ******************
+    dataset = get_dataset(dType=dType, convToTensor = False)
+    split = int(split * len(dataset.data)) # train-test split
+    train_data, test_data = dataset.data[:split], dataset.data[split:]
+    train_target, test_target = dataset.target[:split], dataset.target[split:]
+    if dType == 'letter-features':
+        #trainWordIdxs, testWordIdxs = dataset.wordIdxs[:split], dataset.wordIdxs[split:]
+        train_data, train_target, trainWordLimits = words_to_letters(train_data, train_target)
+        test_data, test_target, testWordLimits =  words_to_letters(test_data, test_target)
+        train_data = np.reshape(train_data, (train_data.shape[0], 1, train_data.shape[1], train_data.shape[2]))
+        test_data  = np.reshape(test_data,  (test_data.shape[0],  1, test_data.shape[1],  test_data.shape[2]))
+    # Convert dataset into torch tensors
+    train = data_utils.TensorDataset(torch.tensor(train_data).float(), torch.tensor(train_target).long())
+    test = data_utils.TensorDataset(torch.tensor(test_data).float(), torch.tensor(test_target).long())
+    # Define train and test loaders
+    trainLoader = data_utils.DataLoader(train,  # dataset to load from
+                                         batch_size=bSize,  # examples per batch (default: 1)
+                                         shuffle=True,
+                                         sampler=None,  # if a sampling method is specified, `shuffle` must be False
+                                         num_workers=5,  # subprocesses to use for sampling
+                                         pin_memory=False,  # whether to return an item pinned to GPU
+                                         )
+
+    testLoader = data_utils.DataLoader(test,  # dataset to load from
+                                        batch_size=bSize,  # examples per batch (default: 1)
+                                        shuffle=False,
+                                        sampler=None,  # if a sampling method is specified, `shuffle` must be False
+                                        num_workers=5,  # subprocesses to use for sampling
+                                        pin_memory=False,  # whether to return an item pinned to GPU
+                                        )
+    print('Loaded dataset... ')
+    # End of DataLoading -------------------
+
+
+    # Sanity Prints---
+    print(testWordLimits[0:200])
+    #print(len(train))
+    #print(type(train[0]))
+    #print(train[0][0].shape)
+    #print(train[0][1].shape)
+    #print(train[0][1].shape)
+    return trainLoader, testLoader, [train_target, test_target, trainWordLimits, testWordLimits, train_data, test_data]
+
+# -------------------------------------------------------------------------------------------------------------
+def get_dataset(dType = 'word-features', convToTensor = False):
+    
+    if dType == 'word-features':
+        dataset = DataLoader(dType=dType)
+        
         # Flatten images into vectors.
         #dataset.data = dataset.data.reshape(dataset.data.shape[:2] + (-1,))
         dataset.data = dataset.data.reshape(dataset.data.shape[:2] + (-1,))
@@ -89,19 +197,27 @@ def get_dataset(type = 'word-features', convToTensor = False):
             if letter:
                 target[index][ord(letter) - ord('a')] = 1
         dataset.target = target
-
-        
+        # Shuffle order of examples.
+        order = np.random.permutation(len(dataset.data))
+        dataset.data = dataset.data[order]
+        dataset.target = dataset.target[order]
     else:
         print("Loading Dataset as Letter-features for Deepnet comparison.\n")
-        dataset = DataLoader(type=type)
-        dataset.data = np.reshape(dataset.data,(dataset.data.shape[0],1,dataset.data.shape[1], dataset.data.shape[2]))
+        dataset = DataLoader(dType=dType)
+        #dataset.data = np.reshape(dataset.data,(dataset.data.shape[0],1,dataset.data.shape[1], dataset.data.shape[2]))
     if convToTensor:
         # Convert dataset into torch tensors
         dataset.data = torch.tensor(dataset.data).float()
         dataset.target = torch.tensor(dataset.target).long()
         
-    # Shuffle order of examples.
-    order = np.random.permutation(len(dataset.data))
-    dataset.data = dataset.data[order]
-    dataset.target = dataset.target[order]
+    
     return dataset
+
+
+def main():
+    #get_dataset(dType = 'letter', convToTensor = False)
+    load_data(dType= 'letter-features')
+    
+    
+if __name__ == '__main__':
+    main()

Assignment_2_Programming/code/dnn_template_fullNet.py
@@ -152,10 +152,11 @@ class Net(nn.Module):
     testLoss = 0
     history = [[],[],[]]
     
+    
     # Mod init + boiler plate code
     # Skeleton of this network; the blocks to be used.
     # Similar to Fischer prize building blocks!
-    def __init__(self, dims = {'conv1':{'nodes':10,'kSize':3, 'stride':1 }, 'conv2':{'nodes':10,'kSize':3, 'stride':1 } }):
+    def __init__(self, device = device, dims = {'conv1':{'nodes':10,'kSize':3, 'stride':1 }, 'conv2':{'nodes':10,'kSize':3, 'stride':1 } }):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, dims['conv1']['nodes'], kernel_size=dims['conv1']['kSize'])
         # Compute dims after 1nd conv layer
@@ -172,6 +173,7 @@ class Net(nn.Module):
         #self.fc1 = nn.Linear(dims['conv2']['nodes'] * h4*w4, 50)
         self.fc1 = nn.Linear(160, 50)
         self.fc2 = nn.Linear(50, 26)
+        self.device = device
 
     # ------------------
 
@@ -203,9 +205,9 @@ class Net(nn.Module):
         x = F.log_softmax(x, dim=1)
         return x
     # ------------------
-    def predict(self, x):
+    def get_predictions(self, x):
         x = self.forward_no_drop(x)
-        x = torch.argmax(x, dim = 1)
+        x = torch.max(x, dim = 1)[1]
         return x
     # ------------------
 
@@ -225,7 +227,9 @@ class Net(nn.Module):
             output      = self.forward_no_drop(data)
             # compute loss
             #loss        = F.cross_entropy(output, label.squeeze())
-            loss        = F.nll_loss(output, label.squeeze())
+            if label.shape[0] > 1:
+                label = label.squeeze()
+            loss = F.nll_loss(output, label)
 
             # Backpropagation part
             # 1. Zero out Grads
@@ -237,7 +241,7 @@ class Net(nn.Module):
 
            # Training Progress report for sanity purposes! 
             if verbose:
-                if idx % 20 == 0: 
+                if idx % 200 == 0: 
                     print("Epoch: {}->Batch: {} / {}. Loss = {}".format(args, idx, len(indata), loss.item() ))
         # Log the current train loss
         self.trainLoss = loss   
@@ -275,12 +279,26 @@ class Net(nn.Module):
         acc = true/len(testLoader.dataset)
         self.accuracy = acc
         self.testLoss = loss
-        self.history[1].append(loss)
+        self.history[1].append(loss/len(testLoader.dataset))
         self.history[2].append(acc)
         # Print accuracy report!
         print("Accuracy: {} ({} / {})".format(acc, true,
                                               len(testLoader.dataset)))
 
+        # Testing and error reports are done here
+    def predict(self, device, testLoader):
+        print("In  Function!")        
+        loss = 0 
+        true = 0
+        acc  = 0
+        # Inform Pytorch that keeping track of gradients is not required in
+        # testing phase.
+        with torch.no_grad():
+            for data, label in testLoader:
+                data, label = data.to(device), label.to(device)
+                output = self.get_redicitons(data)
+                
+        
     def report(self):
 
         print("Current stats of MNIST_NET:")
@@ -288,14 +306,57 @@ class Net(nn.Module):
         print("Training Loss: {}" .format(self.trainLoss))
         print("Test Loss:     {}" .format(self.testLoss))
 
-    def plot(figType = 'acc'):
-        fig = plt.figure()
-        plt.title('Test Accuracy vs Epoch')
-        plt.xlabel('Epochs')
-        plt.xlabel('Accuracy')
-        yAxis = np.arange(len(self.history[2]))+1
-        plt.plot(self.history[2], yAxis)
-        return fig
+    def plot(self,figType = 'acc', saveFile = None):
+        if figType == 'acc':
+            fig = plt.figure()
+            plt.title('Test Accuracy vs Epoch')
+            plt.xlabel('Epochs')
+            plt.ylabel('Accuracy')
+            xAxis = np.arange(len(self.history[2]))+1
+            plt.plot(xAxis, self.history[2], marker = 'o', label="Training Loss")
+            plt.plot(xAxis, self.history[0], marker = 'o')
+            if saveFile is not None:
+                plt.savefig(saveFile)
+            return fig
+
+        if figType == 'lrCurve':
+            fig = plt.figure()
+            plt.title('Learning Curves')
+            plt.xlabel('Epochs')
+            plt.ylabel('Loss')
+            xAxis = np.arange(len(self.history[2]))+1
+            plt.plot(xAxis, self.history[2], marker = 'o', label="Training Loss")
+            plt.plot(xAxis, self.history[0], marker = 'o', label = 'Test Loss')
+            if saveFile is not None:
+                plt.savefig(saveFile)
+            return fig
+    
+    def evaluate(self, dataLoaders, kwArgs = {'lr':0.001, 'm':0.9, 'bSize':32, 'epochs':12}):
+        """ DESCRIPTION: THis function will fit and test on hte given dataloader and the given kwargs.
+                         It stores training and testing information in the model class variables, along
+                         with learned weights.
+            ARGUMENTS: dataLoaders (list of dataloaders) : Tensor dataloader objects for train and test
+                       kwArgs (dict): dicitionary holding the training and testing parameters
+        """
+        
+        # Get args
+        lr, m , bSize, epochs = kwArgs['lr'], kwArgs['m'], kwArgs['bSize'], kwArgs['epochs']
+        dev = self.device
+        # Load data, initialize model and optimizer!
+        trainLoader, testLoader = dataLoaders[0], dataLoaders[1]
+        optim = optm.SGD(self.parameters(), lr=lr, momentum=m)
+
+        print("######### Initiating Network Training and Testing #########\n")
+        print("Parameters: lr:{}, momentum:{}, batch Size:{}, epochs:{}".format(lr,m,bSize,epochs))
+        print("Procedure carried on device: {}\n".format(dev))
+        for e in range(epochs):
+            print("Epoch: {} start ------------\n".format(e))
+            args = e
+            self.train(args, dev, trainLoader, optim)
+            self.test(dev, testLoader)
+
+        # Final report
+        self.report()
         
 # --------------------------------------------------------------------------------------------------------
 
@@ -329,7 +390,7 @@ def main():
     # Get keyboard arguments, if any! (Try the dictionary approach in the code aboe for some practice!)
     lr, m , bSize, epochs = parse_args()
     # Load data, initialize model and optimizer!
-    trainLoader, testLoader = load_data(bSize=bSize)
+    trainLoader, testLoader, _ = load_data(bSize=bSize)
     model = Net().to(device)
     optim = optm.SGD(model.parameters(), lr=lr, momentum=m)
 